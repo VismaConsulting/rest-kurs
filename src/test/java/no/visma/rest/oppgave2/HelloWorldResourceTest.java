312cd85d2607151627efdb5235eaff618529e921
andersem <andersems@gmail.com>
2015-04-21 11:21:29 +0200

    Løsningsforslag til oppgave 1 og 2

--- src/test/java/no/visma/rest/oppgave2/HelloWorldResourceTest.java
+++ src/test/java/no/visma/rest/oppgave2/HelloWorldResourceTest.java
@@ -22,4 +22,13 @@ public class HelloWorldResourceTest {
 
         assertThat(svar).isGreaterThanOrEqualTo(1);
     }
+
+    @Test
+    public void hello_world_oeker_med_2_hver_gang() throws Exception {
+        final WebTarget resource = resources.client().target("/hello-world");
+
+        final Integer svar = resource.request().get(Integer.class);
+        final Integer svar2 = resource.request().get(Integer.class);
+        assertThat(svar2).isEqualTo(svar + 2);
+    }
 }
